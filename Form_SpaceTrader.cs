93013f67b782362e06317f32ecafa7a1bcbb0171
Jay French <jay@frenchfryz.com>
2008-08-13 04:00:43 +0000

    JAF - organized usings

--- Form_SpaceTrader.cs
+++ Form_SpaceTrader.cs
@@ -23,9 +23,8 @@
  *
  ******************************************************************************/
 using System;
-using System.Drawing;
 using System.Collections;
-using System.ComponentModel;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using Microsoft.Win32;
